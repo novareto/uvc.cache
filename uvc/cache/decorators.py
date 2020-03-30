f03e142febf89906db1ef7731473cca6884190b2
Christian Klinger <ck@novareto.de>
2020-03-30 15:11:41 +0200

    add the getStore to a later version on startup tweaking the loglevel to see something

--- uvc/cache/decorators.py
+++ uvc/cache/decorators.py
@@ -11,33 +11,36 @@ logger = logging.getLogger('uvc.cache')
 
 
 def cache_me(marshaller, store_name='redis', lifetime=600):
-    store = getUtility(IStore, name=store_name)
+
+    def getStore():
+        return getUtility(IStore, name=store_name)
 
     def cache(func):
         @wraps(func)
         def cache_replacement(*args, **kwargs):
+            store = getStore()
             key = marshaller(func, *args, **kwargs)
             value = store.get(key)
             if value is DEFAULT_VALUE:
                 value = func(*args, **kwargs)
                 store.set(key, value, delta=lifetime)
-                logger.debug('set value %s with key %s' % (value, key))
+                logger.info('set value %s with key %s' % (value, key))
             else:
-                logger.debug('get value %s from cache with key %s' % (value, key))
+                logger.info('get value %s from cache with key %s' % (value, key))
             return value
         return cache_replacement
     return cache
 
 
 def remove_from_cache(marshaller, store_name='redis'):
-    store = getUtility(IStore, name=store_name)
 
     def invalidate(func):
         @wraps(func)
         def invalidate_replacement(*args, **kwargs):
+            store = getUtility(IStore, name=store_name)
             key = marshaller(func, *args, **kwargs)
             deleted = store.delete(key)
-            logger.debug('remove key %s from store' % key)
+            logger.info('remove key %s from store' % key)
             return func(*args, **kwargs)
         return invalidate_replacement
     return invalidate
